c2dfe73b1cf4b16cd4ed44ae526ac60e82947f3e
jesusmiguelgarcia <jesusmiguelgarcia@gmail.com>
2022-11-28 15:30:58 -0700

    agrego archivo

--- prueba.php
+++ prueba.php
@@ -1,3 +1,16 @@
 <?php
 echo "Holassss444";
+
+/**
+ * hola function
+ *
+ * @param [int] $var1
+ * @param [int] $var2
+ * @return void
+ */
+function hola($var1, $var2)
+{
+    print("holas");
+}
+
 ?>
